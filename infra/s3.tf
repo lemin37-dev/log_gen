82b73e1850eab3dd1f6136bf32d03006979fa75c
lemin37-dev <lemin37.dev@gmail.com>
2026-08-20 13:33:23 +0900

    Feat: S3 버킷명 수정

--- infra/s3.tf
+++ infra/s3.tf
@@ -2,7 +2,7 @@
 # 브론즈/실버/골드 데이터가 저장될 데이터레이크 정의
 resource "aws_s3_bucket" "data" {
   # 버킷 1개 구성, 하위에 브론즈/실버/골드 레이어 관리
-  bucket = "${var.project_name}-loggen-s3-bk-${data.aws_caller_identity.current.account_id}"
+  bucket = "${var.project_name}-s3-bk-${data.aws_caller_identity.current.account_id}"
 
   # 버킷 삭제 옵션 (저장된 객체가 있어도 같이 삭제)
   force_destroy = true
